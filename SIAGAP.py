38df37f4fbd4ab92859b54e960e1f2c5739ba1ed
Guilherme <contato.guilhermealves@gmail.com>
2020-12-01 01:47:59 -0300

    Add files via upload

--- SIAGAP.py
+++ SIAGAP.py
@@ -17,6 +17,7 @@ from tkinter import ttk
 import os
 import tkFileDialog
 import sqlite3
+import xlwt
 
 # banco de dados
 
@@ -34,6 +35,7 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS CORES_CDP (
 v_configura_tela_inicial = 0
 v_cores = 0
 v_cores_cdp = 0
+v_cores_cdp_modo = ''
 
 resolucao_tela = '640x320'
 resolucao_width = 640
@@ -60,6 +62,15 @@ def cores():
 		v_cores = 0	
 	def cores_cdp():
 		global v_cores_cdp
+		def limpar():
+			global v_cores_cdp_modo
+			
+			etrSigla['state'] = 'normal'
+			v_cores_cdp_modo = 'INCLUSAO'
+			etrSigla.delete(0, tk.END)
+			etrDescricao.delete(0, tk.END)
+			lblcor_v['bg'] = '#FFFFFF'
+			etrOrdenacao.delete(0, tk.END)
 		def on_close_cdp():
 			global v_cores_cdp
 			v_cores_cdp = 0
@@ -69,27 +80,30 @@ def cores():
 		def apaga_mensagem():
 			lblMensagem['text'] = ''
 		def consiste():
+			global v_cores_cdp_modo
+			
 			v_sigla = etrSigla.get()
 			v_ordenacao = etrOrdenacao.get()
-			lblMensagem['fg'] = 'red'
+			lblMensagem['fg'] = 'red'		
 			
-			if v_sigla == '':				
-				lblMensagem['text'] = '*PREENCHA A SIGLA'
-				return 0
-			
-			if len(v_sigla) > 1:							
-				lblMensagem['text'] = '*A SIGLA DEVE TER APENAS UM CARACTERE'
-				return 0
-				
-			dbRollNo = ""
-			Select="select SIGLA from CORES_CDP where SIGLA='%s'" %(v_sigla)
-			cursor.execute(Select)
-			result = cursor.fetchall()
-			for i in result:
-				dbRollNo=i[0]
-			if(v_sigla == dbRollNo):
-				lblMensagem['text'] = '*REGISTRO JÁ EXISTE'
-				return 0
+			if v_cores_cdp_modo == 'INCLUSAO':
+				if v_sigla == '':
+					lblMensagem['text'] = '*PREENCHA A SIGLA'
+					return 0
+					
+				if len(v_sigla) > 1:
+					lblMensagem['text'] = '*A SIGLA DEVE TER APENAS UM CARACTERE'
+					return 0
+					
+				dbRollNo = ""
+				Select="select SIGLA from CORES_CDP where SIGLA='%s'" %(v_sigla)
+				cursor.execute(Select)
+				result = cursor.fetchall()
+				for i in result:
+					dbRollNo=i[0]
+				if(v_sigla == dbRollNo):
+					lblMensagem['text'] = '*REGISTRO JÁ EXISTE'
+					return 0
 				
 			if v_ordenacao == '':
 				lblMensagem['text'] = '*PREENCHA A ORDENACAO'
@@ -97,29 +111,121 @@ def cores():
 				
 			return 1
 		def incluir():
+			global v_cores_cdp_modo
+			
 			r = consiste()
 			if bool(r):
-				Insert=''' Insert into CORES_CDP(SIGLA, DESCRICAO, COR, ORDENACAO) values(?,?,?,?) '''
-				Sigla = etrSigla.get()
-				Descricao = etrDescricao.get()
-				Cor = lblcor_v['bg']
-				Ordenacao = etrOrdenacao.get()
-				Value=(Sigla, Descricao, Cor, Ordenacao)
-				cursor.execute(Insert, Value)
-				con.commit()
-				lblMensagem['fg'] = '#2D8C2B'
-				lblMensagem['text'] = '*REGISTRO INCLUÍDO COM SUCESSO'
-				etrSigla.delete(0, tk.END)
-				etrDescricao.delete(0, tk.END)
-				lblcor_v['bg'] = 'white'
-				etrOrdenacao.delete(0, tk.END)
-				t.after(5000, apaga_mensagem)
+				etrSigla['state'] = 'normal'
+				
+				if v_cores_cdp_modo == 'INCLUSAO':
+					Insert=''' Insert into CORES_CDP(SIGLA, DESCRICAO, COR, ORDENACAO) values(?,?,?,?) '''
+					Sigla = etrSigla.get()
+					Descricao = etrDescricao.get()
+					Cor = lblcor_v['bg']
+					Ordenacao = etrOrdenacao.get()
+					Value=(Sigla, Descricao, Cor, Ordenacao)
+					cursor.execute(Insert, Value)
+					con.commit()
+					lblMensagem['fg'] = '#2D8C2B'
+					lblMensagem['text'] = '*REGISTRO INCLUÍDO COM SUCESSO'
+					etrSigla.delete(0, tk.END)
+					etrDescricao.delete(0, tk.END)
+					lblcor_v['bg'] = 'white'
+					etrOrdenacao.delete(0, tk.END)
+					etrSigla.focus()
+					t.after(5000, apaga_mensagem)
+				if v_cores_cdp_modo == 'EDICAO':
+					sigla = etrSigla.get()
+					descricao = etrDescricao.get()
+					cor = lblcor_v['bg']
+					ordenacao = etrOrdenacao.get()
+					Update = "update CORES_CDP set SIGLA='%s', DESCRICAO='%s', COR='%s', ORDENACAO='%s' where sigla='%s'" %(sigla, descricao, cor, ordenacao, sigla)
+					cursor.execute(Update)
+					con.commit()
+					lblMensagem['fg'] = '#2D8C2B'
+					lblMensagem['text'] = '*REGISTRO ATUALIZADO COM SUCESSO'
+					etrSigla.delete(0, tk.END)
+					etrDescricao.delete(0, tk.END)
+					lblcor_v['bg'] = 'white'
+					etrOrdenacao.delete(0, tk.END)
+					etrSigla.focus()
+					t.after(5000, apaga_mensagem)
+					v_cores_cdp_modo = 'INCLUSAO'
 		def consultar():
+			selecionado = {
+			"sigla": "",
+			"descrição": "",
+			"cor": "",
+			"ordenação": ""
+			}	
 			def on_close_cdp_consultas():
 				u.grab_release()
 				t.grab_set()
+			def seleciona(event):
+				global selecionado
+				curItem = treev.focus()
+				selecionado = treev.item(curItem)['values']				
+			def confirmar():
+				global selecionado
+				global v_cores_cdp_modo
+								
+				etrSigla['state'] = 'normal'
+				etrSigla.delete(0, tk.END)				
+				etrSigla.insert(0, selecionado[0])
+				etrSigla['state'] = 'disabled'
+				
+				etrDescricao.delete(0, tk.END)
+				etrDescricao.insert(0, selecionado[1])
+				
+				lblcor_v['bg'] = selecionado[2]
+				
+				etrOrdenacao.delete(0, tk.END)
+				etrOrdenacao.insert(0, selecionado[3])
+				
+				v_cores_cdp_modo = 'EDICAO'
+				u.destroy()
+			def exportar():
+				workbook = xlwt.Workbook()
+				sheet = workbook.add_sheet("Sheet Name")
+				query = "select SIGLA, DESCRICAO, COR, ORDENACAO from CORES_CDP"
+				cursor.execute(query)
+				resultado = cursor.fetchall()
+				sigla = ""
+				descricao = ""
+				cor = ""
+				ordenacao = ""
+				
+				k = 0
+				for i in resultado:
+					sigla = i[0]
+					descricao = i[1]
+					cor = i[2]
+					ordenacao = i[3]
+					
+					RGB_1 = tuple(int(cor.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))[0]
+					RGB_2 = tuple(int(cor.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))[1]
+					RBG_3 = tuple(int(cor.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))[2]
+					
+					xlwt.add_palette_colour("custom_colour" + str(k), 0x21 + k)
+					workbook.set_colour_RGB(0x21 + k, RGB_1, RGB_2, RBG_3)
+					
+					font = xlwt.Font()
+					font.name = 'Times New Roman'				
+					
+					style = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour' + str(k))
+					style.font = font
+																			
+					sheet.write(k, 0, sigla)
+					sheet.write(k, 1, descricao)
+					sheet.write(k, 2, cor, style)
+					sheet.write(k, 3, ordenacao)
+					k = k + 1
+				 
+				workbook.save("sample.xls")																			
 			u = tk.Toplevel()
 			u.grab_set()
+			footer = tk.Frame(u, height=30)
+			footer.pack(fill='both', side='bottom')
 			u.resizable(width = 1, height = 1)
 			treev = ttk.Treeview(u, selectmode ='browse')
 			treev.pack(side ='right')
@@ -136,6 +242,13 @@ def cores():
 			treev.heading("2", text ="DESCRIÇÂO")
 			treev.heading("3", text ="COR")
 			treev.heading("4", text ="ORDENAÇÃO")
+			treev.bind("<<TreeviewSelect>>", seleciona)
+			botaoFechar = Button(footer, text='FECHAR', command=combine_funcs(on_close_cdp_consultas, u.destroy))
+			botaoFechar.pack(side=tk.RIGHT, padx = 5, pady = 5)
+			botaoSelecionar = Button(footer, text='SELECIONAR', command=confirmar)
+			botaoSelecionar.pack(side=tk.RIGHT, padx = 5, pady = 5)
+			botaoExportar = Button(footer, text='EXPORTAR', command=exportar)
+			botaoExportar.pack(side=tk.RIGHT, padx = 5, pady = 5)
 			u.geometry(resolucao_tela_cadastro)
 			u.title('Consulta cores - CDP')
 			u.protocol("WM_DELETE_WINDOW", combine_funcs(on_close_cdp_consultas, u.destroy))
@@ -164,6 +277,9 @@ def cores():
 			u.attributes("-toolwindow", True)
 			u.attributes("-topmost", True)																	
 		if v_cores_cdp == 0:
+			global v_cores_cdp_modo
+			v_cores_cdp_modo = 'INCLUSAO'
+			
 			t = tk.Toplevel()
 			t.grab_set()
 			t.geometry(resolucao_tela_cadastro)
@@ -197,7 +313,9 @@ def cores():
 			botaoIncluir = Button(footer, image=v_cores_cdp_icon_img_2, relief=FLAT, command=incluir)
 			botaoIncluir.grid(column=0, row=8)
 			botaoPesquisar = Button(footer, image=v_cores_cdp_icon_img_3, relief=FLAT, command=consultar)
-			botaoPesquisar.grid(column=1, row=8)	
+			botaoPesquisar.grid(column=1, row=8)
+			botaoLimpar = Button(footer, image=v_cores_cdp_icon_img_4, relief=FLAT, command=limpar)
+			botaoLimpar.grid(column=2, row=8)	
 			t.protocol("WM_DELETE_WINDOW", combine_funcs(on_close_cdp, t.destroy))
 			t.iconbitmap(default='transparent.ico')
 			windowWidth, windowHeight = resolucao_width_cadastro, resolucao_height_cadastro
@@ -283,6 +401,7 @@ icone3 = Image.open("EXIT_64x64.png")
 v_cores_cdp_icon_1 = Image.open("COLOR_16x16.png")
 v_cores_cdp_icon_2 = Image.open("ADD_16x16.png")
 v_cores_cdp_icon_3 = Image.open("SEARCH_16x16.png")
+v_cores_cdp_icon_4 = Image.open("CLEAR_16x16.png")
  
 # Cria Imagens
 imagem1 = ImageTk.PhotoImage(icone1)
@@ -291,6 +410,7 @@ imagem3 = ImageTk.PhotoImage(icone3)
 v_cores_cdp_icon_img_1 = ImageTk.PhotoImage(v_cores_cdp_icon_1)
 v_cores_cdp_icon_img_2 = ImageTk.PhotoImage(v_cores_cdp_icon_2)
 v_cores_cdp_icon_img_3 = ImageTk.PhotoImage(v_cores_cdp_icon_3)
+v_cores_cdp_icon_img_4 = ImageTk.PhotoImage(v_cores_cdp_icon_4)
  
 # Cria botões
 botao1 = Button(ferramenta, image=imagem1,relief=FLAT, command=cores)
