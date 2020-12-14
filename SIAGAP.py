bd7105116b96eff5b27b782c849c2a7a56ca577b
Guilherme <contato.guilhermealves@gmail.com>
2020-12-14 03:15:32 -0300

    Add files via upload
    Novo DBGrid

--- SIAGAP.py
+++ SIAGAP.py
@@ -9,11 +9,12 @@ except:
     import Tkinter as tk
 
 from PIL     import Image, ImageTk
-from Tkinter import Tk,    Frame  
+from Tkinter import Tk,    Frame, Label, Message, StringVar, Canvas  
 from Tkinter import Label, Button, LEFT, TOP, X, FLAT, RAISED, SUNKEN
 from tkinter.colorchooser import askcolor
 from tkinter import ttk
 from tkinter import messagebox
+from Tkconstants import *
 
 import os
 import tkFileDialog
@@ -42,13 +43,13 @@ resolucao_tela = '640x320'
 resolucao_width = 640
 resolucao_height = 320
 
-resolucao_tela_cadastro = '400x250'
+resolucao_tela_cadastro = '400x240'
 resolucao_width_cadastro = 400
-resolucao_height_cadastro = 250
+resolucao_height_cadastro = 240
 
-resolucao_tela_mensagem = '600x300'
-resolucao_width_mensagem = 600
-resolucao_height_mesagem = 300
+resolucao_tela_consulta = '680x320'
+resolucao_width_consulta = 680
+resolucao_height_consulta = 320
 
 # define e implementa as funções
 def client_exit():
@@ -75,27 +76,11 @@ def cores():
 		"descrição": "",
 		"cor": "",
 		"ordenação": ""
-			}	
+			}				
 		def seleciona(event):
 			global selecionado
 			curItem = treev.focus()
-			selecionado = treev.item(curItem)['values']
-		def refresh_consulta():
-			query = "select SIGLA, DESCRICAO, COR, ORDENACAO from CORES_CDP"
-			cursor.execute(query)
-			resultado = cursor.fetchall()
-			sigla = ""
-			descricao = ""
-			cor = ""
-			ordenacao = ""
-			
-			for i in resultado:
-				sigla = i[0]
-				descricao = i[1]
-				cor = i[2]
-				ordenacao = i[3]
-				treev.tag_configure(cor, background=cor)
-				treev.insert("", "end", text = sigla, values=(sigla, descricao, cor, ordenacao), tags = [cor])
+			selecionado = treev.item(curItem)['values']													
 		def novo():
 			global selecionado
 			global v_cores_cdp_modo
@@ -106,6 +91,7 @@ def cores():
 				global v_cores_cdp_modo
 												
 				v_sigla = etrSigla.get()
+				v_descricao = etrDescricao.get()
 				v_ordenacao = etrOrdenacao.get()
 				lblMensagem['fg'] = 'red'
 				
@@ -129,6 +115,11 @@ def cores():
 						lblMensagem['text'] = '*REGISTRO JÁ EXISTE'
 						t.after(5000, apaga_mensagem)
 						return 0
+						
+				if len(v_descricao) > 20:
+					lblMensagem['text'] = '*A DESCRIÇÃO PODE TER APENAS 20 CARACTERES'
+					t.after(5000, apaga_mensagem)
+					return 0
 				
 				if v_ordenacao == '':
 					lblMensagem['text'] = '*PREENCHA A ORDENACAO'
@@ -161,7 +152,7 @@ def cores():
 						lblcor_v['bg'] = 'white'
 						etrOrdenacao.delete(0, tk.END)
 						etrSigla.focus()
-						t.after(4000, apaga_mensagem)
+						t.after(4000, apaga_mensagem)						
 					if v_cores_cdp_modo == 'EDICAO':
 						sigla = etrSigla.get()
 						descricao = etrDescricao.get()
@@ -170,23 +161,24 @@ def cores():
 						Update = "update CORES_CDP set SIGLA='%s', DESCRICAO='%s', COR='%s', ORDENACAO='%s' where sigla='%s'" %(sigla, descricao, cor, ordenacao, sigla)
 						cursor.execute(Update)
 						con.commit()
-						lblMensagem['fg'] = '#2D8C2B'
-						lblMensagem['text'] = '*REGISTRO ATUALIZADO COM SUCESSO'
 						etrSigla.delete(0, tk.END)
 						etrDescricao.delete(0, tk.END)
 						lblcor_v['bg'] = 'white'
 						etrOrdenacao.delete(0, tk.END)
 						etrSigla.focus()
-						t.after(4000, apaga_mensagem)
 						v_cores_cdp_modo = 'INCLUSAO'
-			def modificaModoCadastro(value):
+					combine_funcs(on_close_novo(), t.destroy())				
+			def modificaModoCadastro(value):				
 				global v_cores_cdp_modo
 				v_cores_cdp_modo = value
 			def apaga_mensagem():
 				lblMensagem['text'] = ''
 			def on_close_novo():
-				treev.delete(*treev.get_children())
-				refresh_consulta()
+				global v_cores_cdp_modo
+				
+				v_cores_cdp_modo = 'INCLUSAO'			
+				popular_grid()
+				u.grab_set()																						
 			def limpa():
 				global v_cores_cdp_modo
 				
@@ -197,19 +189,6 @@ def cores():
 				lblcor_v['bg'] = '#FFFFFF'
 				etrOrdenacao.delete(0, tk.END)
 				etrSigla.focus()
-			def exclui():
-				if v_cores_cdp_modo == 'EDICAO':
-					MsgBox = tk.messagebox.askquestion ('Excluir Cor','Deseja excluir esse registro?',icon = 'warning', parent = t)
-					if MsgBox == 'yes':
-						etrSigla['state'] = 'normal'
-						sigla = etrSigla.get()
-						Delete = "delete from CORES_CDP where SIGLA='%s'" %(sigla)
-						cursor.execute(Delete)
-						con.commit()
-						limpa()
-						lblMensagem['fg'] = '#2D8C2B'
-						lblMensagem['text'] = '*REGISTRO EXCLUÍDO COM SUCESSO'
-						t.after(4000, apaga_mensagem)
 			def iniciaEdicao():
 				etrSigla['state'] = 'normal'
 				etrSigla.delete(0, tk.END)
@@ -233,10 +212,6 @@ def cores():
 			def on_enter_botaoLimpar(e):
 				statusbarrodape['text'] = '        LIMPAR'
 			def on_leave_botaoLimpar(e):
-				statusbarrodape['text'] = ''
-			def on_enter_botaoApagar(e):
-				statusbarrodape['text'] = '                EXCLUIR'
-			def on_leave_botaoApagar(e):
 				statusbarrodape['text'] = ''
 			t = tk.Toplevel()
 			t.grab_set()
@@ -300,12 +275,7 @@ def cores():
 			botaoLimpar.grid(column=1, row=8)
 			botaoLimpar.bind("<Enter>", on_enter_botaoLimpar)
 			botaoLimpar.bind("<Leave>", on_leave_botaoLimpar)			
-			
-			botaoApagar = Button(footer, image=v_cores_cdp_icon_img_5, relief=FLAT, command=exclui)
-			botaoApagar.grid(column=2, row=8)
-			botaoApagar.bind("<Enter>", on_enter_botaoApagar)
-			botaoApagar.bind("<Leave>", on_leave_botaoApagar)			
-			
+					
 			statusbarrodape = tk.Label(status, text="", font=("Verdana", 10), anchor=tk.W)
 			statusbarrodape.pack(side=tk.BOTTOM, fill=tk.X)
 			t.protocol("WM_DELETE_WINDOW", combine_funcs(on_close_novo, t.destroy))
@@ -313,21 +283,30 @@ def cores():
 			windowWidth, windowHeight = resolucao_width_cadastro, resolucao_height_cadastro
 			positionRight = int(t.winfo_screenwidth()/2 - windowWidth/2)
 			positionDown = int(t.winfo_screenheight()/2 - windowHeight/2)
-			t.geometry("+{}+{}".format(positionRight, positionDown))
+			t.geometry("+{}+{}".format(positionRight, positionDown))			
 			if v_cores_cdp_modo == 'EDICAO':
 				iniciaEdicao()
+			if etrSigla['state'] == 'normal':
+				etrSigla.focus()
+				
 			t.attributes("-toolwindow", True)
 			t.attributes("-topmost", True)
-		def edicao():
+		def criacao():
 			global selecionado
 			global v_cores_cdp_modo
 			
-			curItem = treev.focus()
-			selecionado = treev.item(curItem)['values']
+			selecionado = ()
+			v_cores_cdp_modo = 'INCLUSAO'
+			novo()
+		def edicao(sigla, descricao, cor, ordenacao):
+			global selecionado
+			global v_cores_cdp_modo
+			
+			selecionado = (sigla, descricao, cor, ordenacao)
 			
 			if selecionado <> '':
 				v_cores_cdp_modo = 'EDICAO'
-				novo()
+				novo()			
 		def exportar():
 			workbook = xlwt.Workbook()
 			sheet = workbook.add_sheet("Sheet Name")
@@ -365,45 +344,162 @@ def cores():
 				sheet.write(k, 3, ordenacao)
 				k = k + 1
 				 
-			workbook.save("sample.xls")		
+			workbook.save("sample.xls")
+		def popular_grid():
+			global selecionado
+			
+			COORDS_LIST = []
+			buttons_dict = {}
+			
+			columns = 6									
+			query = "select SIGLA, DESCRICAO, COR, ORDENACAO, (SELECT COUNT(SIGLA) FROM CORES_CDP) AS LINHAS from CORES_CDP"
+			cursor.execute(query)
+			resultado = cursor.fetchall()
+			sigla = ""
+			descricao = ""
+			cor = ""
+			ordenacao = ""
+			
+			k = 0
+									
+			for i in resultado:
+				k = k + 1
+				for j in range(0, columns):
+					coord = str(k)+"_"+str(j)
+					COORDS_LIST.append(coord)
+					
+					try:
+						widget = frame_buttons.grid_slaves(row=k, column=j)[0]
+						widget.destroy()
+					except:
+						pass
+						
+					try:
+						widget = buttons_dict[COORDS_LIST[-1]].grid_slaves(row=k, column=j)[0]
+						widget.destroy()
+					except:
+						pass
+						
+					sigla = i[0]
+					descricao = i[1]
+					cor  = i[2]
+					ordenacao = i[3]
+					linhas = i[4]
+									
+					if j == 4:						
+						buttons_dict[COORDS_LIST[-1]] = tk.Button(frame_buttons, text='EDITAR', font=("Verdana", 8))
+						buttons_dict[COORDS_LIST[-1]]["command"] = lambda vSigla = sigla, vDescricao = descricao, vCor = cor, vOrdenacao = ordenacao: edicao(vSigla, vDescricao, vCor, vOrdenacao)
+						buttons_dict[COORDS_LIST[-1]].grid(row = k, column = j, sticky = 'news')
+					elif j == 5:						
+						buttons_dict[COORDS_LIST[-1]] = tk.Button(frame_buttons, text='EXCLUIR', font=("Verdana", 8))
+						buttons_dict[COORDS_LIST[-1]]["command"] = lambda vSigla = sigla, vLinhas = linhas + 1, vColunas = columns: exclui(vSigla, vLinhas, vColunas)
+						buttons_dict[COORDS_LIST[-1]].grid(row = k, column = j, sticky = 'news')						
+					else:
+						b = tk.Label(frame_buttons, anchor=W, text='', borderwidth=2, relief="groove")
+						
+						if j == 0:
+							b['text'] = i[0]
+							b['width'] = 10
+						if j == 1:
+							b['text'] = i[1]
+							b['width'] = 30
+						if j == 2:
+							b['bg'] = i[2]
+							b['width'] = 15
+						if j == 3:
+							b['text'] = i[3]
+							b['width'] = 11
+						if j == 4:
+							b['width'] = 10						
+						b.grid(row=k, column=j, sticky='news')		
+										
+			# Update buttons frames idle tasks to let tkinter calculate buttons sizes
+			frame_buttons.update_idletasks()
+		def exclui(sigla, linhas, colunas):
+			MsgBox = tk.messagebox.askquestion ('Excluir Cor','Deseja excluir esse registro?',icon = 'warning', parent = u)
+			if MsgBox == 'yes':			
+				for i in range(1, linhas):
+					for j in range(0, colunas):
+						try:						
+							widget = frame_buttons.grid_slaves(row = i, column = j)[0]
+							widget.destroy()							
+						except:
+							pass						
+						
+						try:							
+							widget = buttons_dict[COORDS_LIST[-1]].grid_slaves(row = i, column = j)[0]
+							widget.destroy()
+						except:
+							pass						
+				
+				Delete = "delete from CORES_CDP where SIGLA='%s'" %(sigla)
+				cursor.execute(Delete)
+				con.commit()				
+						
+				popular_grid()														
 		u = tk.Toplevel()
 		u.grab_set()
-		footer = tk.Frame(u, height=30)
-		footer.pack(fill='both', side='bottom')
-		u.resizable(width = 1, height = 1)
-		treev = ttk.Treeview(u, selectmode ='browse')
-		treev.pack(side ='right')
-		verscrlbar = ttk.Scrollbar(u, orient ="vertical", command = treev.yview)
-		verscrlbar.pack(side ='right', fill ='x')
-		treev.configure(xscrollcommand = verscrlbar.set)
-		treev["columns"] = ("1", "2", "3", "4")
-		treev['show'] = 'headings'
-		treev.column("1", width = 90, anchor ='c')
-		treev.column("2", width = 90, anchor ='se')
-		treev.column("3", width = 90, anchor ='se')
-		treev.column("4", width = 90, anchor ='se')
-		treev.heading("1", text ="SIGLA")
-		treev.heading("2", text ="DESCRIÇÂO")
-		treev.heading("3", text ="COR")
-		treev.heading("4", text ="ORDENAÇÃO")
-		treev.bind("<<TreeviewSelect>>", seleciona)
-		refresh_consulta()
-		botaoFechar = Button(footer, text='FECHAR', command=u.destroy)
-		botaoFechar.pack(side=tk.RIGHT, padx = 5, pady = 5)
-		botaoEditar = Button(footer, text='EDITAR', command=edicao)
-		botaoEditar.pack(side=tk.RIGHT, padx = 5, pady = 5)
-		botaoExportar = Button(footer, text='EXPORTAR', command=exportar)
-		botaoExportar.pack(side=tk.RIGHT, padx = 5, pady = 5)
-		botaoNovo = Button(footer, text='NOVO', command=novo)
-		botaoNovo.pack(side=tk.RIGHT, padx = 5, pady = 5)
-		u.geometry(resolucao_tela_cadastro)
-		u.title('Consulta cores - CDP')
-		u.protocol("WM_DELETE_WINDOW", u.destroy)
-		u.iconbitmap(default='transparent.ico')					
-		windowWidth, windowHeight = resolucao_width_cadastro, resolucao_height_cadastro
-		positionRight = int(u.winfo_screenwidth()/2 - windowWidth/2)
-		positionDown = int(u.winfo_screenheight()/2 - windowHeight/2)
-		u.geometry("+{}+{}".format(positionRight, positionDown))
+		u.geometry(resolucao_tela_consulta)
+		u.title('Consulta Cores - Cores CDP')
+		
+		frame_top = tk.Frame(u)
+		frame_top.grid(sticky='news', padx=1)
+		
+		frame_main = tk.Frame(u)
+		frame_main.grid(sticky='news')
+		
+		label1 = tk.Label(frame_top, text="SIGLA", fg="black", font=("Verdana", 8, 'bold'), width=9, relief="groove", anchor=W)
+		label1.grid(row=0, column=0)
+		
+		label2 = tk.Label(frame_top, text="DESCRIÇÃO", fg="black", font=("Verdana", 8, 'bold'), width = 26, relief="groove", anchor=W)
+		label2.grid(row=0, column=1)
+		
+		label3 = tk.Label(frame_top, text="COR", fg="black", font=("Verdana", 8, 'bold'), width=13, relief="groove", anchor=W)
+		label3.grid(row=0, column=2)
+		
+		label4 = tk.Label(frame_top, text="ORDENAÇÃO", fg="black", font=("Verdana", 8, 'bold'), relief="groove", anchor=W)
+		label4.grid(row=0, column=3)
+	
+		# Create a frame for the canvas with non-zero row&column weights
+		frame_canvas = tk.Frame(frame_main)
+		frame_canvas.grid(row=1, column=0, pady=(5, 0), sticky='nw')
+		frame_canvas.grid_rowconfigure(0, weight=1)
+		frame_canvas.grid_columnconfigure(0, weight=1)
+		# Set grid_propagate to False to allow 5-by-5 buttons resizing later
+		frame_canvas.grid_propagate(False)
+		
+		# Add a canvas in that frame
+		canvas = tk.Canvas(frame_canvas, bg = "#FFFFFF")
+		canvas.grid(row=0, column=0, sticky="news")
+		
+		# Link a scrollbar to the canvas
+		vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
+		vsb.grid(row=0, column=1, sticky='ns')
+		canvas.configure(yscrollcommand=vsb.set)
+		
+		# Create a frame to contain the buttons
+		frame_buttons = tk.Frame(canvas, bg="#FFFFFF")
+		canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
+				
+		popular_grid()
+				
+		# Resize the canvas frame to show exactly 5-by-5 buttons and the scrollbar
+				
+		frame_canvas.config(width=680, height=260)
+		
+		# Set the canvas scrolling region
+		canvas.config(scrollregion=canvas.bbox("all"))
+		
+		frame_bottom = tk.Frame(u)
+		frame_bottom.grid(sticky='news', padx=1)
+		
+		botaoNovo = tk.Button(frame_bottom, text='NOVO', font=("Verdana", 8), command=lambda : criacao())
+		botaoNovo.grid(row=2, column=0)
+
+		windowWidth, windowHeight = resolucao_width_consulta, resolucao_height_consulta
+		positionRight = int(t.winfo_screenwidth()/2 - windowWidth/2)
+		positionDown = int(t.winfo_screenheight()/2 - windowHeight/2)
+		u.geometry("+{}+{}".format(positionRight, positionDown))			
 		u.attributes("-toolwindow", True)
 		u.attributes("-topmost", True)		
 	if v_cores == 0:
@@ -481,7 +577,6 @@ icone2 = Image.open("TOOLS_64x64.png")
 icone3 = Image.open("EXIT_64x64.png")
 v_cores_cdp_icon_1 = Image.open("COLOR_16x16.png")
 v_cores_cdp_icon_2 = Image.open("ADD_32x32.png")
-v_cores_cdp_icon_3 = Image.open("SEARCH_16x16.png")
 v_cores_cdp_icon_4 = Image.open("CLEAR_32x32.png")
 v_cores_cdp_icon_5 = Image.open("DELETE_32x32.png")
  
@@ -491,7 +586,6 @@ imagem2 = ImageTk.PhotoImage(icone2)
 imagem3 = ImageTk.PhotoImage(icone3)
 v_cores_cdp_icon_img_1 = ImageTk.PhotoImage(v_cores_cdp_icon_1)
 v_cores_cdp_icon_img_2 = ImageTk.PhotoImage(v_cores_cdp_icon_2)
-v_cores_cdp_icon_img_3 = ImageTk.PhotoImage(v_cores_cdp_icon_3)
 v_cores_cdp_icon_img_4 = ImageTk.PhotoImage(v_cores_cdp_icon_4)
 v_cores_cdp_icon_img_5 = ImageTk.PhotoImage(v_cores_cdp_icon_5)
  
